faa40c1fe46c19fc91c32df8413e09610f201dd1
AnotherTest <ali.mpfard@gmail.com>
2019-07-16 03:51:21 +0430

    modify commit.sh to use local citron version

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-jq ".version[4]+=1 | .version[5]=\"$(git rev-parse --verify HEAD)\"" citron_standard_library.version -c > out
+jq "$(citron -e "Reflect version split: '-', head split: '.', imap: \\:i:x '.version[\${{i}}\$]=\$\$x', join: '|'")|.version[4]+=1 | .version[5]=\"$(git rev-parse --verify HEAD)\"" citron_standard_library.version -c > out
 mv out citron_standard_library.version
 git add citron_standard_library.version
 git commit
